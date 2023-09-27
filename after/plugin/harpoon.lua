e1fca2811803a683d92f25efa06f2d2f0f742981
Andre Castelo <andrecastelo@gmail.com>
2023-09-27 20:05:16 -0300

    feat: map <leader>j and <leader>k to harpoon prev and next

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_next() end)
+vim.keymap.set("n", "<leader>j", function() ui.nav_prev() end)
+
