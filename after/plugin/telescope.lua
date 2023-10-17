83ecb0f29da256fcee6b3c00398ac98e68496cd3
Andre Castelo <andrecastelo@gmail.com>
2023-10-17 14:22:25 -0300

    feat: add search selected text in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,18 @@
 local builtin = require("telescope.builtin")
 
+function vim.getVisualSelection()
+    vim.cmd('noau normal! "vy"')
+    local text = vim.fn.getreg('v')
+    vim.fn.setreg('v', {})
+
+    text = string.gsub(text, "\n", "")
+    if #text > 0 then
+        return text
+    else
+        return ''
+    end
+end
+
 vim.keymap.set("n", "<leader>pf", function()
     builtin.find_files({
         find_command = {
@@ -21,7 +34,17 @@ vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set("v", "<leader>ps", function()
+    local text = vim.getVisualSelection()
+    builtin.grep_string({ default_text = text })
+end)
+
 vim.keymap.set("n", "<leader>pr", builtin.live_grep)
+vim.keymap.set("v", "<leader>pr", function()
+    local text = vim.getVisualSelection()
+    builtin.live_grep({ default_text = text })
+end)
+
 vim.keymap.set("n", "<leader>b", function()
     builtin.buffers({ show_all_buffers = true })
 end)
