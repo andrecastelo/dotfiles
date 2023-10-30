5818172360b6411ad25768b8302722cbed049151
Andre Castelo <andrecastelo@gmail.com>
2023-10-30 15:11:55 -0300

    remove <leader>pv calling :Ex and change buff next/prev keymaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -28,6 +26,8 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-l>", ":bnext<CR>")
+vim.keymap.set("n", "<C-h>", ":bprev<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>:bd<CR>")
@@ -36,14 +36,11 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
--- split windows using arrow keys by pressing:
+-- change window sizes using arrow keys by pressing:
 -- UP, CTRL+DOWN, CTRL+LEFT, or CTRL+RIGHT.
 vim.keymap.set("n", "<C-S-down>", "<c-w>+")
 vim.keymap.set("n", "<C-S-up>", "<c-w>-")
 vim.keymap.set("n", "<C-S-right>", "10<c-w>>")
 vim.keymap.set("n", "<C-S-left>", "10<c-w><")
-
-vim.keymap.set("n", "<Tab>", ":bnext<CR>")
-vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
 
 vim.keymap.set("n", "<leader>h", ":NvimTreeFindFileToggle<CR>")
