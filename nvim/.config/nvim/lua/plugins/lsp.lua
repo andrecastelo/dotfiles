885397171052285131d7ee7fc5e8d6c3fc87f600
Andre Castelo <andrecastelo@gmail.com>
2024-01-24 11:31:48 -0300

    update lsp config for ruff

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -38,6 +38,7 @@ return {
 
             -- python
             "pyright",
+            "ruff_lsp",
 
             -- golang
             "gopls",
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,8 +1,3 @@
-local utils = require("config.utils")
-local excluded_paths = {
-    "lib/python%d.%d+/site-packages/",
-}
-
 return {
     "nvimtools/none-ls.nvim",
     config = function()
@@ -16,104 +11,6 @@ return {
 
         null_ls.setup({
             -- python
-            diagnostics.flake8.with({
-                diagnostics_format = diagnostics_format,
-                prefer_local = ".venv/bin",
-                cwd = nhelpers.cache.by_bufnr(function(params)
-                    return require("null-ls.utils").root_pattern(
-                        ".flake8",
-                        "pyproject.toml",
-                        "setup.py",
-                        "setup.cfg",
-                        "requirements.txt",
-                        "Pipfile",
-                        "pyrightconfig.json"
-                    )(params.bufname)
-                end),
-                runtime_condition = nhelpers.cache.by_bufnr(function()
-                    local bufname = vim.api.nvim_buf_get_name(0)
-                    for _, path in pairs(excluded_paths) do
-                        if bufname:match(path) ~= nil then
-                            return false
-                        end
-                    end
-
-                    local pyproject = utils.find_file("pyproject.toml")
-                    if pyproject then
-                        local file = assert(io.open(pyproject, "r"))
-                        local content = file:read("*all")
-                        if string.find(content, "tool.ruff") then
-                            return false
-                        end
-                    end
-                    return true
-                end),
-            }),
-            formatting.isort.with({
-                prefer_local = ".venv/bin",
-                cwd = nhelpers.cache.by_bufnr(function(params)
-                    return require("null-ls.utils").root_pattern(
-                        ".isort.cfg",
-                        "pyproject.toml",
-                        "setup.py",
-                        "setup.cfg",
-                        "requirements.txt",
-                        "Pipfile",
-                        "pyrightconfig.json"
-                    )(params.bufname)
-                end),
-                extra_args = { "--profile", "black" },
-                runtime_condition = nhelpers.cache.by_bufnr(function()
-                    local bufname = vim.api.nvim_buf_get_name(0)
-                    for _, path in pairs(excluded_paths) do
-                        if bufname:match(path) ~= nil then
-                            return false
-                        end
-                    end
-
-                    local pyproject = utils.find_file("pyproject.toml")
-                    if pyproject then
-                        local file = assert(io.open(pyproject, "r"))
-                        local content = file:read("*all")
-                        if string.find(content, "tool.ruff.isort") then
-                            return false
-                        end
-                    end
-                    return true
-                end),
-            }),
-            formatting.black.with({
-                prefer_local = ".venv/bin",
-                cwd = nhelpers.cache.by_bufnr(function(params)
-                    return require("null-ls.utils").root_pattern(
-                        "pyproject.toml",
-                        "setup.py",
-                        "setup.cfg",
-                        "requirements.txt",
-                        "Pipfile",
-                        "pyrightconfig.json"
-                    )(params.bufname)
-                end),
-                extra_args = { "--fast" },
-                runtime_condition = nhelpers.cache.by_bufnr(function()
-                    local bufname = vim.api.nvim_buf_get_name(0)
-                    for _, path in pairs(excluded_paths) do
-                        if bufname:match(path) ~= nil then
-                            return false
-                        end
-                    end
-
-                    local pyproject = utils.find_file("pyproject.toml")
-                    if pyproject then
-                        local file = assert(io.open(pyproject, "r"))
-                        local content = file:read("*all")
-                        if string.find(content, "tool.ruff.format") then
-                            return false
-                        end
-                    end
-                    return true
-                end),
-            }),
             diagnostics.ruff.with({
                 diagnostics_format = diagnostics_format,
                 prefer_local = ".venv/bin",
@@ -126,24 +23,6 @@ return {
                         "Pipfile",
                         "pyrightconfig.json"
                     )(params.bufname)
-                end),
-                runtime_condition = nhelpers.cache.by_bufnr(function()
-                    local bufname = vim.api.nvim_buf_get_name(0)
-                    for _, path in pairs(excluded_paths) do
-                        if bufname:match(path) ~= nil then
-                            return false
-                        end
-                    end
-
-                    local pyproject = utils.find_file("pyproject.toml")
-                    if pyproject then
-                        local file = assert(io.open(pyproject, "r"))
-                        local content = file:read("*all")
-                        if string.find(content, "tool.ruff") then
-                            return true
-                        end
-                    end
-                    return false
                 end),
             }),
             formatting.ruff.with({
@@ -158,17 +37,6 @@ return {
                         "pyrightconfig.json"
                     )(params.bufname)
                 end),
-                runtime_condition = nhelpers.cache.by_bufnr(function()
-                    local pyproject = utils.find_file("pyproject.toml")
-                    if pyproject then
-                        local file = assert(io.open(pyproject, "r"))
-                        local content = file:read("*all")
-                        if string.find(content, "tool.ruff") then
-                            return true
-                        end
-                    end
-                    return false
-                end),
                 extra_args = { "--unfixable", "T20,ERA001,F841" },
             }),
             formatting.ruff_format.with({
@@ -182,17 +50,6 @@ return {
                         "Pipfile",
                         "pyrightconfig.json"
                     )(params.bufname)
-                end),
-                runtime_condition = nhelpers.cache.by_bufnr(function()
-                    local pyproject = utils.find_file("pyproject.toml")
-                    if pyproject then
-                        local file = assert(io.open(pyproject, "r"))
-                        local content = file:read("*all")
-                        if string.find(content, "tool.ruff.format") then
-                            return true
-                        end
-                    end
-                    return false
                 end),
             }),
 
@@ -249,7 +106,7 @@ return {
             formatting.yamlfix.with({
                 env = function(params)
                     return {
-                        YAMLFIX_LINE_LENGTH = tostring(vim.opt_local.textwidth:get() + 1),
+                        -- YAMLFIX_LINE_LENGTH = tostring(vim.opt_local.textwidth:get() + 1),
                         YAMLFIX_SECTION_WHITELINES = "1",
                         YAMLFIX_quote_representation = '"',
                         YAMLFIX_SEQUENCE_STYLE = "block_style",
