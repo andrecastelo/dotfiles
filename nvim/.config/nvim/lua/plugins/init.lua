b6da5bf5b3ca640e49c66f9bb0d2f8a0883b958f
Andre Castelo <andrecastelo@gmail.com>
2024-02-14 15:18:12 -0300

    organize plugins alphabetically

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,12 @@
 return {
     {
-        "rose-pine/neovim",
-        as = "rose-pine",
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons",
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        event = "VimEnter",
     },
     {
         "folke/tokyonight.nvim",
@@ -20,42 +25,6 @@ return {
             })
             vim.cmd.colorscheme("tokyonight")
         end,
-    },
-    "nvim-treesitter/playground",
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        enabled = false,
-        config = function()
-            require("treesitter-context").setup({
-                multiline_threshold = 1,
-            })
-        end
-    },
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 500
-            require("which-key").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            })
-        end,
-    },
-    "mbbill/undotree",
-    "tpope/vim-fugitive",
-    {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = "nvim-tree/nvim-web-devicons",
-    },
-    { "lewis6991/gitsigns.nvim" },
-    { "numToStr/Comment.nvim" },
-    {
-        "glepnir/dashboard-nvim",
-        event = "VimEnter",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     {
         "folke/trouble.nvim",
@@ -87,15 +56,46 @@ return {
             end)
         end,
     },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 500
+            require("which-key").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            })
+        end,
+    },
+    {
+        "glepnir/dashboard-nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    { "IndianBoy42/tree-sitter-just" },
+    { "lewis6991/gitsigns.nvim" },
+    { "mbbill/undotree" },
+    { "numToStr/Comment.nvim" },
+    { "nvim-treesitter/playground" },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        enabled = false,
+        config = function()
+            require("treesitter-context").setup({
+                multiline_threshold = 1,
+            })
+        end
+    },
+    {
+        "nvim-tree/nvim-web-devicons",
+    },
+    { "rose-pine/neovim",  as = "rose-pine" },
+    { "tpope/vim-fugitive" },
+    { "tpope/vim-rhubarb" },
     -- {
     --     "windwp/nvim-autopairs",
     --     event = "InsertEnter",
     --     opts = {},
     -- },
-    {
-        "christoomey/vim-tmux-navigator",
-        event = "VimEnter",
-    },
-    { "tpope/vim-rhubarb" },
-    { "IndianBoy42/tree-sitter-just" },
 }
