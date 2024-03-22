6c0376e32a7ceb48c00765babf8ea26786e508f4
Andre Castelo <andrecastelo@gmail.com>
2024-03-22 14:29:47 -0300

    feat: add wezterm configs

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font = wezterm.font('FiraCode Nerd Font')
+config.font_size = 15
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+config.use_dead_keys = true
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
