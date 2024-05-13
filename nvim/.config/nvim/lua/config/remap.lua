1c01722b944b89e703d300d4f41b8d23a9b6d9f8
Andre Castelo <andrecastelo@gmail.com>
2024-05-13 14:34:15 -0300

    Ctrl+D and Ctrl+U dont work with vscode

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -49,8 +49,6 @@ if not vim.g.vscode then
     })
 else
     vim.keymap.set("n", "J", "mzJ`z")
-    vim.keymap.set("n", "<C-d>", "<C-d>zz")
-    vim.keymap.set("n", "<C-u>", "<C-u>zz")
     vim.keymap.set("n", "n", "nzzzv")
     vim.keymap.set("n", "N", "Nzzzv")
     vim.keymap.set("n", "Q", "<nop>")
