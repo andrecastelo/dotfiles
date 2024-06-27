ad93d37c7b7a004b1892141a27a54ac72f879f1b
Andre Castelo <andrecastelo@gmail.com>
2024-06-27 16:28:35 -0300

    chore: remove unused null-ls formatters

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,56 +5,11 @@ return {
         -- https://github.com/nvimtools/none-ls.nvim
         local diagnostics_format = "[#{c}] #{m} (#{s})"
         local null_ls = require("null-ls")
-        local nhelpers = require("null-ls.helpers")
         local formatting = null_ls.builtins.formatting
         local diagnostics = null_ls.builtins.diagnostics
-        local code_actions = null_ls.builtins.code_actions
 
         null_ls.setup({
             sources = {
-                -- python
-                -- diagnostics.ruff.with({
-                --     diagnostics_format = diagnostics_format,
-                --     prefer_local = ".venv/bin",
-                --     cwd = nhelpers.cache.by_bufnr(function(params)
-                --         return require("null-ls.utils").root_pattern(
-                --             "pyproject.toml",
-                --             "setup.py",
-                --             "setup.cfg",
-                --             "requirements.txt",
-                --             "Pipfile",
-                --             "pyrightconfig.json"
-                --         )(params.bufname)
-                --     end),
-                -- }),
-                -- formatting.ruff.with({
-                --     prefer_local = ".venv/bin",
-                --     cwd = nhelpers.cache.by_bufnr(function(params)
-                --         return require("null-ls.utils").root_pattern(
-                --             "pyproject.toml",
-                --             "setup.py",
-                --             "setup.cfg",
-                --             "requirements.txt",
-                --             "Pipfile",
-                --             "pyrightconfig.json"
-                --         )(params.bufname)
-                --     end),
-                --     extra_args = { "--unfixable", "T20,ERA001,F841" },
-                -- }),
-                -- formatting.ruff_format.with({
-                --     prefer_local = ".venv/bin",
-                --     cwd = nhelpers.cache.by_bufnr(function(params)
-                --         return require("null-ls.utils").root_pattern(
-                --             "pyproject.toml",
-                --             "setup.py",
-                --             "setup.cfg",
-                --             "requirements.txt",
-                --             "Pipfile",
-                --             "pyrightconfig.json"
-                --         )(params.bufname)
-                --     end),
-                -- }),
-
                 -- javascript/typescript
                 formatting.prettier.with({
                     prefer_local = "node_modules/.bin",
@@ -72,9 +27,6 @@ return {
                         return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
                     end,
                 }),
-
-                -- refactoring
-                code_actions.refactoring,
 
                 -- yaml
                 diagnostics.yamllint.with({
