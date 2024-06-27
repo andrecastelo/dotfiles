92b46f38c02d4470287aea686af312b3a0512529
Andre Castelo <andrecastelo@gmail.com>
2024-06-27 17:13:14 -0300

    fix: identation

--- nvim/.config/nvim/after/ftplugin/javascript.lua
+++ nvim/.config/nvim/after/ftplugin/javascript.lua
@@ -1,6 +1,5 @@
 if not vim.g.vscode then
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-end 
-
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+end
