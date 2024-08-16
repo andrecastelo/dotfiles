e9d515f720bf1a7070e869d2fb3a0c473d19fe26
André Castelo <andrecastelo@gmail.com>
2024-08-15 21:29:27 -0300

    chore: update lsp to 4.x

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,40 +1,36 @@
 return {
-    'stevearc/conform.nvim',
-    enabled = not vim.g.vscode,
-    opts = {},
-    config = function()
-        local conform = require('conform');
+	"stevearc/conform.nvim",
+	enabled = not vim.g.vscode,
+	opts = {},
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = 'fallback',
-            },
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                graphql = { "prettier" },
+		conform.setup({
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				svelte = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				graphql = { "prettierd" },
 
-                lua = { "stylua" },
+				lua = { "stylua" },
 
-                python = { "ruff_organize_imports", "ruff_format" },
+				python = { "ruff_organize_imports", "ruff_format" },
 
-                go = { "goimports", "gofmt" },
-            },
-        })
+				go = { "goimports", "gofmt" },
+			},
+		})
 
-        vim.api.nvim_create_user_command(
-            "Format",
-            require('andrecastelo.utils').format,
-            { range = true }
-        )
-    end
+		vim.api.nvim_create_user_command("Format", require("andrecastelo.utils").format, { range = true })
+	end,
 }
