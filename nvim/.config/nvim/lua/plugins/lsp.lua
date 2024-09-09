b4d02e5e407f76858efcf4130e82b83de8220f27
André Castelo <andrecastelo@gmail.com>
2024-09-09 16:10:04 -0300

    fix: issue where eslint would not find eslint.config.js

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -137,6 +137,18 @@ return {
         })
 
         lspconfig.eslint.setup({
+            root_dir = lspconfig.util.root_pattern(
+                '.eslintrc',
+                '.eslintrc.js',
+                '.eslintrc.cjs',
+                '.eslintrc.yaml',
+                '.eslintrc.yml',
+                '.eslintrc.json',
+                'eslint.config.js'
+            -- Disabled to prevent "No ESLint configuration found" exceptions
+            -- 'package.json',
+            ),
+
             on_attach = function(client, bufnr)
                 on_attach(client, bufnr)
                 vim.api.nvim_create_autocmd("BufWritePre", {
