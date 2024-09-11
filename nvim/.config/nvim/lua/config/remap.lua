2f18f47d77c94422925c1562e9b5668dde0a351d
André Castelo <andrecastelo@gmail.com>
2024-09-11 17:01:01 -0300

    trying to make `gx` work, but no luck. so this is just a formatting pass

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -1,69 +1,69 @@
 vim.g.mapleader = " "
 
 if not vim.g.vscode then
-	local wk = require("which-key")
-	wk.add({
-		-- which keys
-		{ "<C-h>", "<cmd> TmuxNavigateLeft<CR>", desc = "window left" },
-		{ "<C-j>", "<cmd> TmuxNavigateUp<CR>", desc = "window up" },
-		{ "<C-k>", "<cmd> TmuxNavigateDown<CR>", desc = "window down" },
-		{ "<C-l>", "<cmd> TmuxNavigateRight<CR>", desc = "window right" },
-		{ "J", "mzJ`z", desc = "move the line below to the end of the current line" },
-		{ "<C-d>", "<C-d>zz" },
-		{ "<C-u>", "<C-u>zz" },
-		{ "n", "nzzzv" },
-		{ "N", "Nzzzv" },
-		{ "Q", "<nop>", desc = "no op" },
-		{ "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>", desc = "Open projects in a new session" },
-		{
-			"<leader>f",
-			function()
-				require("andrecastelo.utils").format()
-			end,
-			desc = "format the file",
-		},
-		{
-			"<leader>s",
-			[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-			desc = "search and replace the word under the cursor",
-		},
-		{ "<leader>Y", [["+Y]], desc = "copy to outside clipboard" },
-		{ "<C-b>", ":NvimTreeFindFileToggle<CR>", desc = "toggle nvim-tree" },
-		{ "<leader>h", ":Oil<CR>", desc = "open oil" },
-		-- { "<leader>h", ":NvimTreeFindFileToggle<CR>", desc = "open the nvim tree" },
+    local wk = require("which-key")
+    wk.add({
+        -- which keys
+        { "<C-h>", "<cmd> TmuxNavigateLeft<CR>",           desc = "window left" },
+        { "<C-j>", "<cmd> TmuxNavigateUp<CR>",             desc = "window up" },
+        { "<C-k>", "<cmd> TmuxNavigateDown<CR>",           desc = "window down" },
+        { "<C-l>", "<cmd> TmuxNavigateRight<CR>",          desc = "window right" },
+        { "J",     "mzJ`z",                                desc = "move the line below to the end of the current line" },
+        { "<C-d>", "<C-d>zz" },
+        { "<C-u>", "<C-u>zz" },
+        { "n",     "nzzzv" },
+        { "N",     "Nzzzv" },
+        { "Q",     "<nop>",                                desc = "no op" },
+        { "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>", desc = "Open projects in a new session" },
+        {
+            "<leader>f",
+            function()
+                require("andrecastelo.utils").format()
+            end,
+            desc = "format the file",
+        },
+        {
+            "<leader>s",
+            [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+            desc = "search and replace the word under the cursor",
+        },
+        { "<leader>Y", [["+Y]],                       desc = "copy to outside clipboard" },
+        { "<C-b>",     ":NvimTreeFindFileToggle<CR>", desc = "toggle nvim-tree" },
+        { "<leader>h", ":Oil<CR>",                    desc = "open oil" },
+        -- { "<leader>h", ":NvimTreeFindFileToggle<CR>", desc = "open the nvim tree" },
 
-		-- change window sizes using arrow keys by pressing:
-		-- UP, CTRL+DOWN, CTRL+LEFT, or CTRL+RIGHT.
-		{ "<S-up>", "<c-w>+", desc = "increase current pane height" },
-		{ "<S-down>", "<c-w>-", desc = "decrease current pane height" },
-		{ "<S-left>", "10<c-w>>", desc = "decrease current pane width" },
-		{ "<S-right>", "10<c-w><", desc = "increase current pane width" },
-	}, { mode = "n" })
+        -- change window sizes using arrow keys by pressing:
+        -- UP, CTRL+DOWN, CTRL+LEFT, or CTRL+RIGHT.
+        { "<S-up>",    "<c-w>+",                      desc = "increase current pane height" },
+        { "<S-down>",  "<c-w>-",                      desc = "decrease current pane height" },
+        { "<S-left>",  "10<c-w>>",                    desc = "decrease current pane width" },
+        { "<S-right>", "10<c-w><",                    desc = "increase current pane width" },
+    }, { mode = "n" })
 
-	wk.add({
-		{ "<", "<gv", desc = "Continuous visual indenting", mode = "v" },
-		{ ">", ">gv", desc = "Continuous visual indenting", mode = "v" },
+    wk.add({
+        { "<",         "<gv",              desc = "Continuous visual indenting",                        mode = "v" },
+        { ">",         ">gv",              desc = "Continuous visual indenting",                        mode = "v" },
 
-		-- greatest remap ever
-		{ "p", [["_dP]], desc = "Pastes without removing the data from the register", mode = "x" },
+        -- greatest remap ever
+        { "p",         [["_dP]],           desc = "Pastes without removing the data from the register", mode = "x" },
 
-		-- next greatest remap ever : asbjornHaland
-		{ "<leader>y", [["+y]], desc = "Yanks to the global register", mode = { "n", "v" } },
-		{ "<leader>d", [["_d]], desc = "Deletes without copying the deleted stuff", mode = { "n", "v" } },
+        -- next greatest remap ever : asbjornHaland
+        { "<leader>y", [["+y]],            desc = "Yanks to the global register",                       mode = { "n", "v" } },
+        { "<leader>d", [["_d]],            desc = "Deletes without copying the deleted stuff",          mode = { "n", "v" } },
 
-		{ "J", ":m '>+1<CR>gv=gv", desc = "Move line down", mode = "v" },
-		{ "K", ":m '<-2<CR>gv=gv", desc = "Move line up", mode = "v" },
-		{ "<F1>", "<ESC>", mode = { "n", "v", "i" } },
-	})
+        { "J",         ":m '>+1<CR>gv=gv", desc = "Move line down",                                     mode = "v" },
+        { "K",         ":m '<-2<CR>gv=gv", desc = "Move line up",                                       mode = "v" },
+        { "<F1>",      "<ESC>",            mode = { "n", "v", "i" } },
+    })
 else
-	vim.keymap.set("n", "J", "mzJ`z")
-	vim.keymap.set("n", "n", "nzzzv")
-	vim.keymap.set("n", "N", "Nzzzv")
-	vim.keymap.set("n", "Q", "<nop>")
+    vim.keymap.set("n", "J", "mzJ`z")
+    vim.keymap.set("n", "n", "nzzzv")
+    vim.keymap.set("n", "N", "Nzzzv")
+    vim.keymap.set("n", "Q", "<nop>")
 
-	vim.keymap.set("v", "<", "<gv")
-	vim.keymap.set("v", ">", ">gv")
+    vim.keymap.set("v", "<", "<gv")
+    vim.keymap.set("v", ">", ">gv")
 
-	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+    vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 end
