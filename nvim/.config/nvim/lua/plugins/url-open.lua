1ed9af74b179d5cd8710af3464aa3919bd94b044
André Castelo <andrecastelo@gmail.com>
2024-09-11 17:01:25 -0300

    url-open

---
+++ nvim/.config/nvim/lua/plugins/url-open.lua
@@ -0,0 +1,17 @@
+return {
+    "sontungexpt/url-open",
+    enabled = false,
+    event = "VeryLazy",
+    cmd = "URLOpenUnderCursor",
+    config = function()
+        local status_ok, url_open = pcall(require, "url-open")
+        if not status_ok then
+            return
+        end
+        url_open.setup({})
+
+        require("which-key").add({
+            { "gx", "<esc>:URLOpenUnderCursor<cr>" }
+        }, { mode = "n" })
+    end,
+}
