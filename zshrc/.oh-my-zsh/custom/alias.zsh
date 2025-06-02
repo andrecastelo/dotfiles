b5c5288651c6cc8f2311343d06a9f02a4f8c8ec9
André Castelo <andrecastelo@gmail.com>
2025-06-02 16:52:22 -0300

    fix: update zshrc aliases

--- zshrc/.oh-my-zsh/custom/alias.zsh
+++ zshrc/.oh-my-zsh/custom/alias.zsh
@@ -9,11 +9,8 @@ alias lg="lazygit"
 
 # project aliases
 alias p="cd ~/Projects"
-alias cap="cd ~/Projects/parade/capacity-ui-v2/parade-capacity-portal"
-alias radar="cd ~/Projects/parade/public-carriers-api/"
-alias codriver="cd ~/Projects/parade/codriver/"
 
 alias dotfiles="nvim ~/dotfiles/"
 alias stage-db="cloud-sql-proxy parade-capacity-stage:us-central1:capacity-api-stage-pg15 --port 2345 capi_stage:i91ArXnHKDp3W9EDCa"
 
-alias nvim-update="mise uninstall nvim@nightly && mise install neovim@nightly"
+alias nvim-update="mise uninstall neovim@nightly && mise install neovim@nightly"
