9f3791d9c37439895caf5965f6c33a0fc354f4bf
André Castelo <andrecastelo@gmail.com>
2025-06-05 16:53:17 -0300

    fix: remove the automatic zR, annoying af

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -1,9 +1,9 @@
 require("config.remap")
 require("config.set")
 
-if not vim.g.vscode then
-    vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
-        pattern = "*",
-        command = "normal zR",
-    })
-end
+-- if not vim.g.vscode then
+--     vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+--         pattern = "*",
+--         command = "normal zR",
+--     })
+-- end
